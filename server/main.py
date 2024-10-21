ee15d04c453dcec0283e2cddf6fcdcdb122a0a65
Satyarth Raghuvanshi <srk.satyarth.1@gmail.com>
2024-10-22 01:12:06 +0530

    🧑‍💻 added `ratelimit lfespan` event in the main server

--- server/main.py
+++ server/main.py
@@ -1,12 +1,33 @@
+from contextlib import asynccontextmanager
+
+import redis.asyncio as redis
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi_limiter import FastAPILimiter
+from redis.asyncio import Redis
 
 from server import server_settings
 from src.genai_process.routes import extraction_router
 from src.sample_paper.routes import sample_paper_router
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    redis_connection = await Redis.from_url(
+        f"redis://{server_settings.REDIS_HOST}:{server_settings.REDIS_PORT}",
+        password=server_settings.REDIS_PASSWORD,
+        encoding="utf-8",
+        decode_responses=True,
+    )
+    await FastAPILimiter.init(redis_connection)
+    yield
+    await FastAPILimiter.close()
+
+
+app = FastAPI(lifespan=lifespan)
+
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
